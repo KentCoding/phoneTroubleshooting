b9ab9c7ac3f628368533a8e9508511cec7fb31f0
SammyHerring <sammy.herring@social-systems.co.uk>
2016-08-18 02:44:36 +0100

    v0.0.2
    Script allows brand entry based upon array, however, exception handler
    not yet written. Next revision will handle errors for brands and allow
    handover to secondary sub procedures for given brands.

--- phoneTroubleshooting.py
+++ phoneTroubleshooting.py
@@ -1,1 +1,26 @@
-print(--Welcome to Phone Troubleshooting--)
+#Import Modules
+import warnings
+import string
+
+#Define Globals
+brands = ["apple", "android", "windows"]
+brand = ''
+
+def Main():
+    init()
+    print("--Welcome to Troubleshooting Applet--")
+    print("In your query please include the brand of your device and the problem / symptom of your current issue. \n")
+    query = input("Enter your query: ").lower()
+    brand = set(brands).intersection(query.split())
+    brand = ', '.join(brand)
+
+    print(brand)
+    
+    if brand in brands:
+        print("Exist")
+
+def init():
+    warnings.filterwarnings("ignore") #Disable while debugging
+
+if __name__ == '__main__':
+    Main()
