e1b1965b99169a640db7644e59c2a4bf8bc9d851
SammyHerring <sammy.herring@social-systems.co.uk>
2016-08-18 02:58:08 +0100

    v0.0.2 r2
    Separated function for brand formatting, exception handler structure
    set but not fully functional - errors not giving error code.

--- phoneTroubleshooting.py
+++ phoneTroubleshooting.py
@@ -1,6 +1,7 @@
 #Import Modules
 import warnings
 import string
+import sys
 
 #Define Globals
 brands = ["apple", "android", "windows"]
@@ -10,14 +11,21 @@ def Main():
     init()
     print("--Welcome to Troubleshooting Applet--")
     print("In your query please include the brand of your device and the problem / symptom of your current issue. \n")
-    query = input("Enter your query: ").lower()
-    brand = set(brands).intersection(query.split())
-    brand = ', '.join(brand)
 
+    query = input("Enter your query: ").lower()
+    brand = brandSelector(query)
     print(brand)
     
     if brand in brands:
         print("Exist")
+
+def brandSelector(query):
+    try: 
+        brand = set(brands).intersection(query.split())
+        brand = ', '.join(brand)
+        return brand
+    except error:
+        print("Brand does not exist")
 
 def init():
     warnings.filterwarnings("ignore") #Disable while debugging
